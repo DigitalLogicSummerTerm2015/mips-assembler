50d28a7eebd57877ea3cf4d18a40e719d6b02d50
Thomas Lee <lisihan969@gmail.com>
2015-07-09 02:59:09 +0800

    Add empty tests

--- test/test_assembler.rb
+++ test/test_assembler.rb
@@ -1,5 +1,17 @@
 require "test_helper"
 require "mips"
 
-class TestAssembler
+# Testcase for the MIPS assembler.
+class TestAssembler < Minitest::Test
+  def test_nop
+  end
+
+  def test_memory_access
+  end
+
+  def test_algebra
+  end
+
+  def test_branch
+  end
 end
