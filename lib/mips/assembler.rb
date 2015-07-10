0a4a50f8543a5128f06b18af23ab3010d9379abe
Thomas Lee <lisihan969@gmail.com>
2015-07-10 21:53:12 +0800

    Record current address.

--- lib/mips/assembler.rb
+++ lib/mips/assembler.rb
@@ -1,10 +1,11 @@
 module MIPS
   # Assembly MIPS codes into machine codes.
   class Assembler
-    attr_reader :symbol_table
+    attr_reader :symbol_table, :current_addr
 
     def initialize
       @symbol_table = {}
+      @current_addr = 0x0
     end
 
     def assembly(line)
