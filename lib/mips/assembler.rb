ca2b00be1d592d21ce32ae47dcaecb77486bcbc6
Thomas Lee <lisihan969@gmail.com>
2015-07-10 22:10:41 +0800

    Implement read_tag.

--- lib/mips/assembler.rb
+++ lib/mips/assembler.rb
@@ -1,4 +1,14 @@
 module MIPS
+  class MIPSSyntaxError < StandardError
+    def initialize(msg)
+      @msg = msg
+    end
+
+    def to_s
+      msg
+    end
+  end
+
   # Assembly MIPS codes into machine codes.
   class Assembler
     attr_reader :symbol_table, :current_addr
@@ -9,11 +19,28 @@ module MIPS
     end
 
     def assembly(line)
-      0x0
+      line.sub!(/#*/, "")  # Remove comments.
+      line.strip!
+
+      read_tag line
+      return if line.empty?  # Tag line.
+
+
     end
 
     def self.assembly(line)
       new.assembly(line)
     end
+
+    private
+
+    def read_tag(line)
+      return unless /^(?<tag>[a-zA-Z]\w*)\s*:\s*(?<rest>.*)/ =~ line
+
+      if @symbol_table.include? tag
+        fail MIPSSyntaxError, "Redeclaration of tag `#{tag}`"
+      else
+        @symbol_table[tag] = @current_addr
+      end
   end
 end
