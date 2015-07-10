7ccf0e14adbfcbcb102f6c6e29b020d4a41b7f6e
Thomas Lee <lisihan969@gmail.com>
2015-07-10 19:33:09 +0800

    Add basic structure for Assembler.

--- lib/mips/assembler.rb
+++ lib/mips/assembler.rb
@@ -1,4 +1,18 @@
 module MIPS
+  # Assembly MIPS codes into machine codes.
   class Assembler
+    attr_reader :symbol_table
+
+    def initialize
+      @symbol_table = {}
+    end
+
+    def assembly(line)
+      0x0
+    end
+
+    def self.assembly(line)
+      new.assembly(line)
+    end
   end
 end
