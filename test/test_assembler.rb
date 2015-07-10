fa0697b620072dc29e2bb534a157eb5cbab14195
Thomas Lee <lisihan969@gmail.com>
2015-07-10 19:21:40 +0800

    Done basic tests

--- test/test_assembler.rb
+++ test/test_assembler.rb
@@ -16,6 +16,7 @@ class TestAssembler < Minitest::Test
     # (lw, sw) rt, offset(rs)
     assert_equal 0x8d280004, asm("lw $t0, 4($t1)")
     assert_equal 0xad280004, asm("sw $t0, 4($t1)")
+
     # lui rt, immi
     assert_equal 0x3c0800ff, asm("lui $t0, 0x00ff")
   end
@@ -31,25 +32,50 @@ class TestAssembler < Minitest::Test
     assert_equal 0x012a4026, asm("xor  $t0, $t1, $t2")
     assert_equal 0x012a4027, asm("nor  $t0, $t1, $t2")
     assert_equal 0x012a402a, asm("slt  $t0, $t1, $t2")
+  end
 
+  def test_algrebra_with_immediate
     # (addi, addiu, andi, slti, sltiu) rt, rs, immi
     assert_equal 0x212800ff, asm("addi  $t0, $t1, 0x00ff")
     assert_equal 0x252800ff, asm("addiu $t0, $t1, 0x00ff")
     assert_equal 0x312800ff, asm("andi  $t0, $t1, 0x00ff")
     assert_equal 0x292800ff, asm("slti  $t0, $t1, 0x00ff")
     assert_equal 0x2d2800ff, asm("sltiu $t0, $t1, 0x00ff")
+  end
 
+  def test_shift
     # (sll, srl, sra) rd, rt, shamt
     assert_equal 0x00094100, asm("sll $t0, $t1, 4")
     assert_equal 0x00094102, asm("srl $t0, $t1, 4")
     assert_equal 0x00094103, asm("sra $t0, $t1, 4")
   end
 
-  def test_branch
+  def test_branch_comparing_two
     # (beq, bne) rs, rt, label
+    assert_equal 0x1109ffff, asm("HERE: beq $t0, $t1, HERE")
+    assert_equal 0x1509ffff, asm("HERE: bne $t0, $t1, HERE")
+  end
+
+  def test_branch_comparing_with_zero
     # (blez, bgtz, bgez) rs, label
+    assert_equal 0x1900ffff, asm("HERE: blez $t0, HERE")
+    assert_equal 0x1d00ffff, asm("HERE: bgtz $t0, HERE")
+    assert_equal 0x0501ffff, asm("HERE: bgez $t0, HERE")
+  end
+
+  def test_normal_jump
     # (j, jal) target
+    assert_equal 0x08000000, asm("HERE: j   HERE")
+    assert_equal 0x0c000000, asm("HERE: jal HERE")
+  end
+
+  def test_jump_register
     # jr rs
+    assert_equal 0x03e00008, asm("jr $ra")
+  end
+
+  def test_jump_link_register
     # jalr rd, rs
+    assert_equal 0x01204009, asm("jalr $t0, $t1")
   end
 end
