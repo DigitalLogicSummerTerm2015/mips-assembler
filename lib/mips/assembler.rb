bbd8df0b208cd12cb903b20e40f5c4ce37ee9098
Thomas Lee <lisihan969@gmail.com>
2015-07-16 04:34:05 +0800

    Pass test_jump_register

--- lib/mips/assembler.rb
+++ lib/mips/assembler.rb
@@ -15,7 +15,7 @@ module MIPS
   }
 
   REGS = %w(zero at v0 v1 a0 a1 a2 a3 t0 t1 t2 t3 t4 t5 t6 t7
-            s0 s1 s2 s3 s4 s5 s6 s7 t8 t9 k0 k1 gp sp fp rs)
+            s0 s1 s2 s3 s4 s5 s6 s7 t8 t9 k0 k1 gp sp fp ra)
 
   # Represent a MIPS syntax error
   class MIPSSyntaxError < StandardError
@@ -100,6 +100,8 @@ module MIPS
         fail MIPSSyntaxError, "Syntax: #{cmd} target" unless arg1 && arg2.nil?
         type_j(cmd_id, absolute_addr(arg1))
       when :jr
+        fail MIPSSyntaxError, "Syntax: jr rs" unless arg1 && arg2.nil?
+        type_r(reg(arg1), 0, 0, 0, cmd_id)
       when :jalr
       else
         fail MIPSSyntaxError, "#{cmd}: Unknown command"
