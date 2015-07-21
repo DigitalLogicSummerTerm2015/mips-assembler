dcdce010410ff8410ca81e0bf01101b885902422
Thomas Lee <lisihan969@gmail.com>
2015-07-21 11:48:41 +0800

    Assume args in $a0 & $a1

--- test/gcd.asm
+++ test/gcd.asm
@@ -68,9 +68,12 @@ Break:
 
     # Calculate GCD.
     addi    $v0, $zero, $zero   # Initialize result to 0
-    # Assume a@$s0, b@$s1
+    # Assume a@$a0, b@$a1.
     beq     $s0, $zero, Done
     beq     $s1, $zero, Done
+    # Copy to $s0, $s1.
+    add     $s0, $a0, $zero
+    add     $s1, $a1, $zero
 
 Loop:
     blt     $s0, $s1, Swap
