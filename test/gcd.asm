a7dabed33cbe5123de3df4e17ca107d4e6889326
Thomas Lee <lisihan969@gmail.com>
2015-07-21 09:56:13 +0800

    Implement gcd

--- test/gcd.asm
+++ test/gcd.asm
@@ -1,4 +1,9 @@
-    # Initialize.
+    # Entries.
+    j       Reset
+    j       Break
+    j       Exception
+
+Reset:
     # Digit tube decoding.
     addi    $t0, $zero, 0x0040  # 0
     sw      $t0, 0($zero)
@@ -33,14 +38,50 @@
     addi    $t0, $zero, 0x000e  # F
     sw      $t0, 60($zero)
 
-    # Timer @ 0x40000000.
-    lui     $s0, 0x4000  # TCON = 0
-    sw      $zero, 8($s0)
+    # Initialize timer @ 0x40000000.
+    lui     $s7, 0x4000
+    sw      $zero, 8($s7)       # TCON = 0
     addi    $t0, $zero, 0x8000  # TH = 0xffff8000
-    sw      $t0, 0($s0)
+    sw      $t0, 0($s7)
     addi    $t0, $zero, 0xffff  # TL = 0xffffffff
-    sw      $t0, 4($s0)
-    addi    $t0, $zero, 3  # TCON = 3
-    sw      $t0, 8($s0)
+    sw      $t0, 4($s7)
+    addi    $t0, $zero, 3       # TCON = 3
+    sw      $t0, 8($s7)
+
+Break:
+    # Disable break & clear status.
+    lui     $s7, 0x4000
+    lw      $t0, 8($s7)         # TCON
+    addi    $t1, $zero, 0xfff9  # $t1 = 0xfffffff9
+    and     $t0, $t0, $t1
+    sw      $t0, 8($s7)         # TCON &= 0xfffffff9
+
+    # Calculate GCD.
+    addi    $v0, $zero, $zero   # Initialize result to 0
+    # Assume a@$s0, b@$s1
+    and     $t0, $s0, $s1
+    beq     $t0, $zero, Done
+
+Loop:
+    blt     $s0, $s1, Swap
+    sub     $s0, $s0, $s1
+    b       Loop
+Swap:
+    add     $t0, $s0, $zero
+    add     $s0, $s1, $zero
+    add     $s1, $t0, $zero
+
+    bne     $s1, $zero, Loop
+    add     $v0, $s0, $zero
+
+Done:
 
 
+    # Enable break.
+    lw      $t0, 8($s7)
+    ori     $t0, $t0, 0x0002    # TCON |= 0x00000002
+
+Return:
+    jr      $k0                 # Jump back
+
+Exception:
